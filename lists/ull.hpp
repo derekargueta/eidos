5db07e81aa027770988e4e17732e6388a9a47a05
Derek Argueta <dereka@pinterest.com>
2017-11-26 00:34:20 -0600

    insertion more accurate

--- lists/ull.hpp
+++ lists/ull.hpp
@@ -6,13 +6,14 @@ template <typename T>
 class UllNode {
 public:
   UllNode(int size) {
+    this->totalCapacity = size;
     this->arr = new T[size];
     this->numElements = 0;
-    this->next = nullptr;
+    this->_next = nullptr;
   }
 
   ~UllNode() {
-    delete this->next;
+    // delete this->_next;
     delete[] this->arr;
   }
 
@@ -20,28 +21,65 @@ public:
     return this->arr[index];
   }
 
+  /**
+   * Places an item at the last available spot in the node's array
+   */
   void push_back(T item) {
     this->arr[this->numElements] = item;
     this->numElements += 1;
   }
 
+  void clearUpperRange() {
+    this->numElements /= 2;
+  }
+
+  // TODO this actually requires moving things in the array
+  void clearLowerRange() {
+    this->numElements /= 2;
+  }
+
+  void setNext(UllNode<T>* newNext) {
+    this->_next = newNext;
+  }
+
+  UllNode<T>* next() {
+    return this->_next;
+  }
+
+  bool isFull() {
+    return this->totalCapacity == this->numElements;
+  }
+
+  int size() {
+    return this->numElements;
+  }
+
 private:
-  UllNode<T>* next;
+  UllNode<T>* _next;
   int numElements;
+  int totalCapacity;
   T* arr;
 };
 
 template <typename T>
 class UnrolledLinkedList : public List<T> {
 public:
+  
+  /**
+   * Default constructor
+   */
   UnrolledLinkedList() {
     this->_len = 0;
     this->_head = nullptr;
+    this->_maxNumElementsPerNode = 16;
   }
+  
   UnrolledLinkedList(int nodeSize) {
     this->_len = 0;
     this->_head = nullptr;
+    this->_maxNumElementsPerNode = nodeSize;
   }
+
   ~UnrolledLinkedList() {
     // TODO: delete all connected nodes, not just head
     delete _head;
@@ -49,26 +87,67 @@ public:
 
   T get(int index) {
     UllNode<T>* tmp = this->_head;
-    while (index > this->_maxNumElementsPerNode) {
-      index -= this->_maxNumElementsPerNode;
+    while (index >= this->_maxNumElementsPerNode) {
+      index -= tmp->size();
+      tmp = tmp->next();
     }
 
     return tmp->get(index);
   }
+
+  /**
+   * Adds an item to the end of the list
+   */
   void push_back(T item) {
     if (this->_head == nullptr) {
       this->_head = new UllNode<T>(this->_maxNumElementsPerNode);
       this->_head->push_back(item);
       this->_len += 1;
     } else {
-      UllNode<T>* tmp = this->_head;
+      UllNode<T>* tmp = this->getLastNode();
+
+      if (!tmp->isFull()) {
+        tmp->push_back(item);
+        this->_len += 1;
+      } else {
+        // TODO move half of elements from `tmp` to `newNode`
+        UllNode<T>* newNode = new UllNode<T>(this->_maxNumElementsPerNode);
+        for (int i = tmp->size() / 2; i < tmp->size(); i++) {
+          newNode->push_back(tmp->get(i));
+        }
+        newNode->push_back(item);
+        tmp->clearUpperRange();
+        tmp->setNext(newNode);
+        this->_len += 1;
+      }
     }
   }
+
   int size() { return this->_len; }
+
+  int numberOfLinks() {
+    int counter = 1;
+    UllNode<T>* tmp = this->_head;
+    while (tmp->next() != nullptr) {
+      tmp = tmp->next();
+      counter += 1;
+    }
+
+    return counter;
+  }
 private:
   int _len;
   int _maxNumElementsPerNode;
   UllNode<T>* _head;
+
+  UllNode<T>* getLastNode() {
+    UllNode<T>* tmp = this->_head;
+    while (tmp->next() != nullptr) {
+      tmp = tmp->next();
+    }
+
+    return tmp;
+  }
 };
 
 
--- tests/test.cpp
+++ tests/test.cpp
@@ -5,8 +5,58 @@
 
 TEST(UnrolledLinkedList, BasicCreation) {
   UnrolledLinkedList<int> ull{};
-  //EXPECT_TRUE(true);
-  EXPECT_TRUE(ull.size() == 0);
+  EXPECT_EQ(ull.size(), 0);
+}
+
+TEST(UnrolledLinkedList, AppendingWithLargeNodes) {
+  UnrolledLinkedList<int> ull{};
+  ull.push_back(5);
+  EXPECT_EQ(ull.size(), 1);
+  EXPECT_EQ(ull.get(0), 5);
+  EXPECT_EQ(ull.numberOfLinks(), 1);
+
+  ull.push_back(1);
+  EXPECT_EQ(ull.size(), 2);
+  EXPECT_EQ(ull.get(1), 1);
+  EXPECT_EQ(ull.numberOfLinks(), 1);
+
+  ull.push_back(3);
+  EXPECT_EQ(ull.size(), 3);
+  EXPECT_EQ(ull.get(2), 3);
+  EXPECT_EQ(ull.numberOfLinks(), 1);
+
+  ull.push_back(9);
+  EXPECT_EQ(ull.size(), 4);
+  EXPECT_EQ(ull.get(3), 9);
+  EXPECT_EQ(ull.numberOfLinks(), 1);
+}
+
+TEST(UnrolledLinkedList, AppendingWithSmallNodes) {
+  UnrolledLinkedList<int> ull(2);
+
+  // [ , ] --> [5, ]
+  ull.push_back(5);
+  EXPECT_EQ(ull.size(), 1);
+  EXPECT_EQ(ull.get(0), 5);
+  EXPECT_EQ(ull.numberOfLinks(), 1);
+
+  // [5, ] --> [5, 1]
+  ull.push_back(1);
+  EXPECT_EQ(ull.size(), 2);
+  EXPECT_EQ(ull.get(1), 1);
+  EXPECT_EQ(ull.numberOfLinks(), 1);
+
+  // [5, 1] --> [5, ]~[1, 2]
+  ull.push_back(3);
+  EXPECT_EQ(ull.size(), 3);
+  EXPECT_EQ(ull.get(2), 3);
+  EXPECT_EQ(ull.numberOfLinks(), 2);
+
+  // [5, ]~[1, 2] --> [5, ]~[1, ]~[2, 3]
+  ull.push_back(9);
+  EXPECT_EQ(ull.size(), 4);
+  EXPECT_EQ(ull.get(3), 9);
+  EXPECT_EQ(ull.numberOfLinks(), 3);
 }
 
 int main(int argc, char **argv) {
